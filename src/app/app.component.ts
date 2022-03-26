8f63ef56bc8bbc337393bffc20b07dd7a6973554
zelimran <elimrani.z@gmail.com>
2022-03-26 19:01:41 +0100

    Finishing the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'cards';
+  
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy mountain',
+      imageUrl: 'mountain.jpeg',
+      username: 'mountainLover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mounting Biking',
+      imageUrl: 'biking.jpeg',
+      username: 'bikingGeeks',
+      content: 'I did some biking today'
+    }
+  ]
+
 }
